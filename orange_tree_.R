7ea31f451a4ab09787f80c15b32ace3510c3c6a8
evilsan134 <elenavilla357@gmail.com>
2022-09-22 13:35:08 +0200

    script Orange Tree

---
+++ orange_tree_.R
@@ -0,0 +1,8 @@
+#Se carga la base de datos y se visualiza el encabezado y las primeras filas
+data("Orange")
+head(Orange)
+
+#Se mira la base de datos
+View(Orange)
+#Contiene 25 observaciones, una variable respuesta (circumference), una variable
+#predictora que es continua (age) y un factor aleatorio (tree) con siete niveles.
